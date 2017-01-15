fd32f4b5f43659bb915596f06f9eee781f9d1632
Shashank kannam <kannamshashank19@gmail.com>
2017-01-15 18:48:06 -0500

    Added geofire, randomly placing pokemons on the map, opens maps when a poken is selected and gives direction

--- CatchThePokemon/ViewController.swift
+++ CatchThePokemon/ViewController.swift
@@ -11,9 +11,12 @@ import MapKit
 import FirebaseDatabase
 
 class ViewController: UIViewController, MKMapViewDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager = CLLocationManager()
+    
+    var mapHasCenteredonce:Bool = false
     
     var geoFire:GeoFire!
     var geoFireRef: FIRDatabaseReference!
@@ -30,9 +33,11 @@ class ViewController: UIViewController, MKMapViewDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    
     override func viewDidAppear(_ animated: Bool) {
         locationAuthorStatus()
     }
+    
     
     func locationAuthorStatus(){
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
@@ -42,28 +47,100 @@ class ViewController: UIViewController, MKMapViewDelegate {
         }
     }
 
+    
     func centreMapOnLocation(location: CLLocation){
-        let coordinate = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        let coordinate = MKCoordinateRegionMakeWithDistance(location.coordinate,2000,2000)
         mapView.setRegion(coordinate, animated: true)
     }
     
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if let loc = userLocation.location {
+            if !mapHasCenteredonce {
+                centreMapOnLocation(location: loc)
+                mapHasCenteredonce = true
+            }
+        }
+
+    }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        var annotationView:MKAnnotationView?
+        let annoIdentifier = "Pokemon"
+        var annotationView: MKAnnotationView?
         
-        if annotation.isKind(of: MKUserLocation.self){
-          annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+        if annotation.isKind(of: MKUserLocation.self) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
             annotationView?.image = UIImage(named: "ash")
+        } else if let deqAnno = mapView.dequeueReusableAnnotationView(withIdentifier: annoIdentifier) {
+            annotationView = deqAnno
+            annotationView?.annotation = annotation
+        } else {
+            let av = MKAnnotationView(annotation: annotation, reuseIdentifier: annoIdentifier)
+            av.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView = av
+        }
+        if let annotationView = annotationView, let anno = annotation as? PokeAnnotation {
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: "\(anno.pokemonNumber)")
+            let btn = UIButton()
+            btn.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            btn.setImage(UIImage(named: "map"), for: .normal)
+            annotationView.rightCalloutAccessoryView = btn
         }
         return annotationView
     }
     
+    
+    func createlocationForPokemon(location: CLLocation, pokeID: Int){
+        geoFire.setLocation(location, forKey: "\(pokeID)")
+    }
+    
+    
+    func showPokemons(location: CLLocation){
+        let circleQuery = geoFire.query(at: location, withRadius: 2.5)
+        
+        _ = circleQuery?.observe(GFEventType.keyEntered, with: { (key, location) in
+            if let key = key, let location = location{
+                let annotation = PokeAnnotation(coordinate: location.coordinate, pokemonNumber: Int(key)!)
+                self.mapView.addAnnotation(annotation)
+            }
+        })
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        let location = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        showPokemons(location: location)
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if let anno = view.annotation as? PokeAnnotation{
+            let place = MKPlacemark(coordinate: anno.coordinate)
+            let destination = MKMapItem(placemark: place)
+            destination.name = "Pokemon location"
+            
+            let regionDistance:CLLocationDistance = 1000
+            let regionSpan = MKCoordinateRegionMakeWithDistance(anno.coordinate, regionDistance, regionDistance)
+            
+            let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span), MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving] as [String : Any]
+            
+            MKMapItem.openMaps(with: [destination], launchOptions: options)
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    
     @IBAction func getPokemons(_ sender: UIButton) {
+        
+        let location = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        
+        let rand = arc4random_uniform(151) + 1
+        
+        createlocationForPokemon(location: location, pokeID: Int(rand))
         
     }
 
